40cd27d0bafd023d49d5df65a7ad30bb5b6c06dc
gggraca <ggraca22@gmail.com>
2025-10-21 16:15:32 +0100

    test for genFragEntry

---
+++ tests/testthat/test-genFragEntry.R
@@ -0,0 +1,26 @@
+context("genFragEntry()")
+
+# remove file created by function
+on.exit( tryCatch({ file.remove('./Pantothenic_acid_pos.csv') }, error=function(e){ invisible() }, warning=function(w){ invisible() }) )
+
+testFile <- system.file("./Libraries/Metabolites/POS/Pantothenic acid_pos.csv",
+                        package = "MetaboAnnotatoR")
+expected <- read.csv(testFile, check.names = FALSE)
+
+test_that("library entry correctly generated", {
+  
+  spec <- system.file("/Data/Pantothenic_acid_pos.txt", package = "MetaboAnnotatoR")
+  specObject <- read.table(spec, header=FALSE)
+  entry <- genFragEntry(specObject, 
+                        "Pantothenic acid",
+                        "[M+H]+",220.1179,
+                        "Pantothenic_acid_pos", 
+                        noise = 0.005, 
+                        mpeaksScore = 0.9, 
+                        mpeaksThres = 0.1, 
+                        mzTol = 0.01)
+  
+  entry <- read.csv("Pantothenic_acid_pos.csv", check.names = FALSE)
+  
+  expect_equal(entry, expected)
+})
