ec72d13a1c6801e38152a2a5671321cb8d387aa8
Gomes Da Graca <ggomesda@ic.ac.uk>
2025-10-30 11:01:39 +0000

    identation corrections

--- tests/testthat/test-RCspec.R
+++ tests/testthat/test-RCspec.R
@@ -1,10 +1,12 @@
+## test for RCspec function
+
 # read test data
-RC <- system.file("/Data/MESA_RAMClustR.rds", package = "MetaboAnnotatoR")
+RCpath <- system.file("/Data/MESA_RAMClustR.rds", package = "MetaboAnnotatoR")
+RC <- readRDS(RCpath)
 expected <- data.frame(
-  mz=c(391.2248, 469.3115, 468.3089, 450.2982, 471.3170, 451.3013),
-  into=c(1015.122, 303973.046, 1279964.631, 36714.991, 7463.412, 8979.110),
-  rt=c(81.577, 81.612, 81.827, 82.351, 82.380, 82.729)
-)
+    mz=c(391.2248, 469.3115, 468.3089, 450.2982, 471.3170, 451.3013),
+    into=c(1015.122, 303973.046, 1279964.631, 36714.991, 7463.412, 8979.110),
+    rt=c(81.577, 81.612, 81.827, 82.351, 82.380, 82.729))
 
 test_that("Correct Pseudo-MS/MS spectrum is retrieved from RAMClustR object", {
   pseudoSpec <- RCspec(fmz=468.3094, frt=82.92, RC) 
