861ed44e147f0fbe126ebfb8ef36c830d526d9bc
Gomes Da Graca <ggomesda@ic.ac.uk>
2025-11-24 19:10:28 +0000

    expanded to include test RC/xcms data

--- R/getDemoData.R
+++ R/getDemoData.R
@@ -4,14 +4,24 @@
 #' to test automated AIF annotation using annotateAIF function.
 #'
 #' @author Goncalo Graca (Imperial College London)
-#'
+#' @param AIFdata If AIF test data is to be loaded. Default is FALSE.
 #' @return Files: targetTable.csv, XCMS_options.csv
 #' @export
-getDemoData <- function() {
+getDemoData <- function(AIFdata=FALSE) {
     targetTablePath <- system.file("targetTable.csv", 
                                    package="MetaboAnnotatoR")
     xcmsOptionsPath <- system.file("XCMS_options.csv", 
                                    package="MetaboAnnotatoR")
     file.copy(from=xcmsOptionsPath, to=getwd())
     file.copy(from=targetTablePath, to=getwd())
+    if(isTRUE(AIFdata)){
+        rcpath <- system.file("/Data/MESA_RAMClustR.rds", 
+                              package = "MetaboAnnotatoR")
+        RC <- readRDS(rcpath)
+        XsetPath <- system.file("/Data/MESA_Xset.rds", 
+                              package = "MetaboAnnotatoR")
+        Xset <- readRDS(XsetPath)
+        data <- list(RC=RC, Xset=Xset)
+        list2env(data, envir = .GlobalEnv)
+    }
 }
