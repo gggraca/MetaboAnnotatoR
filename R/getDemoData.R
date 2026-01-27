f661eeddbf93a43c3f2a1fb087f0a2d3c1442e92
gggraca <ggraca22@gmail.com>
2026-01-27 11:18:47 +0200

    function option removed

--- R/getDemoData.R
+++ R/getDemoData.R
@@ -1,31 +1,18 @@
 #' Function to place demonstration data into working directory.
 #'
 #' Places XCMS_options and target features files into the working directory 
-#' to test automated AIF annotation using annotateAIF function.
+#' to test automated AIF annotation using annotateAIF or annotateRC functions.
 #'
 #' @author Goncalo Graca (Imperial College London)
-#' @param AIFdata If AIF test data is to be loaded. Default is FALSE.
-#' @return Files: targetTable.csv, XCMS_options.csv and two additional 
-#' environment objects if AIFdata=TRUE: Xset (xcmsSet object) and 
-#' RC (RAMClustR object).
+#' @return Files: targetTable.csv, XCMS_options.csv
 #' @examples
 #' getDemoData()
 #' @export
-getDemoData <- function(AIFdata=FALSE) {
+getDemoData <- function() {
     targetTablePath <- system.file("targetTable.csv", 
                                     package="MetaboAnnotatoR")
     xcmsOptionsPath <- system.file("XCMS_options.csv", 
                                     package="MetaboAnnotatoR")
     file.copy(from=xcmsOptionsPath, to=getwd())
     file.copy(from=targetTablePath, to=getwd())
-    if(isTRUE(AIFdata)){
-        rcpath <- system.file("/Data/MESA_RAMClustR.rds", 
-                                package="MetaboAnnotatoR")
-        RC <- readRDS(rcpath)
-        XsetPath <- system.file("/Data/MESA_Xset.rds", 
-                                package="MetaboAnnotatoR")
-        Xset <- readRDS(XsetPath)
-        data <- list(RC=RC, Xset=Xset)
-        list2env(data, envir=.GlobalEnv)
-    }
 }
