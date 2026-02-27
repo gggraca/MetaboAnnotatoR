c6c8832dd6032b5061e493061df2bf63995c3a60
Gomes Da Graca <ggomesda@ic.ac.uk>
2026-02-27 17:21:24 +0000

    test updated to the new outputs of annotateRC

--- tests/testthat/test-annotateRC.R
+++ tests/testthat/test-annotateRC.R
@@ -1,27 +1,36 @@
 ## test for annotateRC function
 
 ## load test data
-tPath <- system.file("targetTable.csv", package = "MetaboAnnotatoR")
+data("RC")
+data("xset")
+
+targets <- data.frame(feature.mz=520.3408533, feature.rt=100.6238759)
+
+expected <- data.frame(feature.mz=520.3408533,
+					   feature.rt=100.6238759,
+					   metabolite="LPC(18:2)",
+					   feature.type="parent",
+					   ion.type="[M+H]+",
+					   isotope="M+0",
+					   mz.metabolite=520.339805,
+					   matched.mz=520.339805,
+					   mz.error=2.01464096744439,
+					   pseudoMSMS="FALSE",
+					   fraction="3  of  4",
+					   score=0.423183179077441
+					   )
 
 test_that("output files are created", {
     # run function  
     suppressMessages(
-      annotateRC(targetTable=tPath, 
-                 xcmsObject=xset, 
-                 ramclustObj=RC, 
-                 libs="Lipids", 
-                 ESImode="POS")
+    	annotations <- annotateRC(targets, 
+                                  xcmsObject=xset, ramclustObj=RC, 
+    							  libs="LipidPos", RTs="none", checkIsotope=TRUE)
       )
-    # test presence of Annotation folder 
-    expect_true(dir.exists("./Annotations/"))
-    # test the size of the result file list
-    resultsPath <- paste("./Annotations/", dir("./Annotations/"), sep="")
-    fileList <- dir(resultsPath)
-    expect_equal(length(fileList), 22)
-    on.exit( tryCatch({ file.remove(fileList) }, 
-                      error=function(e){ invisible() }, 
-                      warning=function(w){ invisible() }))
+	global <- annotations$global
+    # test if annotations object is a list 
+    expect_true(is.list(annotations))
+    
+    # test if expected is equal to annotations$global result object
+    expect_equal(global, expected)
 })
-
-# remove ./Libraries and files
-unlink("Annotations", recursive=TRUE)
