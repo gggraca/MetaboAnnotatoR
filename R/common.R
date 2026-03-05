feb1c6c52d9697ed295561b5ece76e70ba0dd957
Gomes Da Graca <ggomesda@ic.ac.uk>
2026-03-05 20:20:10 +0000

    new plotting function added

--- R/common.R
+++ R/common.R
@@ -1,5 +1,6 @@
 ## Common helper functions to organize annotation results 
 ## Goncalo Graca (Imperial College London)
+## These functions are not exported
 
 ## Load libraries and get libraries filepaths----------------------------------
 loadLibs <- function(libs){
@@ -31,4 +32,47 @@ storeAnnotations <- function(global, rankedResult){
                     "fraction", "score")] <- NA
     }
     return(global)
+}
+
+## Plot pseudo-MS/MS composed of candidate matched ions
+plotResultSpec <- function(annotations, feature, candidate){
+	    rankedResult <- annotations$rankedResult[[feature]]
+	    if(is.null(rankedResult)) {
+	    	message("No annotation result found for this feature.")
+	    } else {
+	    	rankedSpectra <- annotations$rankedSpectra[[feature]]
+	    	# get relevant information from the rankedResult and rankedSpectra objects
+	    	metabolite <- rankedResult[candidate,"metabolite"]
+	    	ionType <- rankedResult[candidate,"ion.type"]
+	    	score <- rankedResult[candidate,"score"]
+	    	adductName <- paste(metabolite, ionType)
+	    	candidateMZ <- round(rankedResult[candidate,"mz.metabolite"], 3)
+	    	MZerror <- round(rankedResult[candidate,"mz.error"], 1)
+	    	rnk <- rankedResult[candidate,"rank"]
+	    	specMatch <- rankedSpectra[[candidate]]
+	    	fmz <- rankedResult[candidate,"feature.mz"]
+	    	frt <- rankedResult[candidate,"feature.rt"]
+	    	
+	    	# plotting part
+	    	if(is.null(nrow(specMatch))) {
+	    		df <- as.data.frame(specMatch[c("mz", "into")])
+	    	} else df <- as.data.frame(specMatch[,c("mz", "into")])
+	    	
+	    	plt <- ggplot2::ggplot(df,
+	    						   ggplot2::aes(x=mz, y=into, label=round(mz, 3))) +
+	    		ggplot2::geom_segment(ggplot2::aes(xend=mz, yend=0), 
+	    							  color="red", lwd=0.5) +
+	    		ggplot2::geom_text(size=3, angle=45, hjust=0, vjust=0) +
+	    		ggplot2::ggtitle(paste("Feature ", round(fmz, 4), "m/z", "_", 
+	    							   round(frt, 4), "s ", ", Rank ", rnk, 
+	    							   " result: ", adductName, ", mz.error = ", 
+	    							   MZerror, " ppm, ", "score = ", 
+	    							   round(score, 2), sep="")) +
+	    		ggplot2::theme_minimal() +
+	    		ggplot2::theme(plot.title=ggplot2::element_text(hjust=0.5)) +
+	    		ggplot2::ylim(0, max(df$into) + 0.1*max(df$into)) +
+	    		ggplot2::xlim(min(df$mz)-50, max(df$mz)+50) +
+	    		ggplot2::labs(x="m/z", y="Intensity (a.u.)")
+	    	plt
+	    }
 }
