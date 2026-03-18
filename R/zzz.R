0d2208b3321813189519cfacf341eb1817e0ab34
Gomes Da Graca <ggomesda@ic.ac.uk>
2026-03-18 13:30:32 +0000

    more global variables added

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,9 @@
                     pkgname = "MetaboAnnotatoR"){
     # R CMD check workaround for ggplot2 variable names
     if(getRversion() >= "2.15.1") {
-        utils::globalVariables(c("mz","rt","into"))
+        utils::globalVariables(c("mz","rt","into",
+                                "LipidPos","LipidNeg",
+                                "MetabolitesPos","MetabolitesNeg"))
     invisible()
     }
 }
